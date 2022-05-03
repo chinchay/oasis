cbc58fc44930bd4aaf0279a2776eb95c23a2fe68
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2022-05-03 17:57:37 -0400

    first commit

---
+++ cloud.py
@@ -0,0 +1,19 @@
+#%% This file is intended to be an interface for interaction with cloud database
+
+def getCityTemperatures():
+    return
+#
+
+def promptCities():
+    return
+#
+
+def getSliceTime():
+    return
+
+#
+
+def signCloud():
+    return
+#
+
